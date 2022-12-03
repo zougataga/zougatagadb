cc9ecd12fdd341fdf70397968b2295b97d2e5be4
zougataga <107312482+zougataga@users.noreply.github.com>
2022-12-03 20:25:09 +0100

    Delete app directory

--- app/result.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    path: `/search`,
-    method: "post",
-    go: async (req, res) => {
-        return res.render("loading.ejs", { h: `/search/${req.body.search}` })
-    }
-}
--- app/root.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    path: `/`,
-    method: "get",
-    go: async (req, res) => {
-       return res.sendFile("index.html", { root: "./views" });
-    }
-}
