a527720d60f362abc5de1758b6eddfae639d735e
zougataga <107312482+zougataga@users.noreply.github.com>
2023-03-26 17:07:08 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -11,31 +11,31 @@ npm i zougatagadb
 ## Example
 
 ```js
-
 const zougatagaDb = require("zougatagadb");
 const db = new zougatagaDb();
 // if you want to specify a path you can do so like this
-// const db = new zougatagaDb({ path: "source/to/path/test.zougatagadb" });
+// const db = new zougatagaDb({ path: "source/to/path/test.db" });
 
 db.set("alllevel", [{ level: 1 }, { level: 2 }]);
-// -> [{ level: 1 }]
+// -> [{ level: 1 }, { level: 2 }]
 
 db.get("alllevel");
-// -> [{ level: 1 }]
+// -> [{ level: 1 }, { level: 2 }]
 
 db.push("alllevel", { level: 1 });
+// -> [{ level: 1 }, { level: 2 }, { level: 1 }]
 
 db.pull("alllevel", (e) => e.level === 1);
-// => [{level: 1}, {level: 1}]
+// -> [{level: 1}, {level: 1}]
 
 db.getAll()
 // -> [ { id: 'alllevel', data: [{ level: 1 }, { level: 2 }, {level: 1}] } ]
 
 db.pullDelete("alllevel", (e) => e.level === 1)
-// => [{level: 2}]
+// -> [{level: 2}]
 
 db.delete("alllevel");
-// => undefined
+// -> undefined
 
 db.set("level_<userId>", 4);
 // -> 4
@@ -47,5 +47,5 @@ db.subtract("level_<userId>", 7);
 // -> -2
 
 db.deleteAll();
-// => undefined
+// -> undefined
 ```
--- example/index.js
+++ example/index.js
@@ -1,28 +1,28 @@
-
-const zougatagaDb = require("../");
+const zougatagaDb = require("zougatagadb");
 const db = new zougatagaDb();
 // if you want to specify a path you can do so like this
-// const db = new zougatagaDb({ path: "source/to/path/test.zougatagadb" });
+// const db = new zougatagaDb({ path: "source/to/path/test.db" });
 
 db.set("alllevel", [{ level: 1 }, { level: 2 }]);
-// -> [{ level: 1 }]
+// -> [{ level: 1 }, { level: 2 }]
 
 db.get("alllevel");
-// -> [{ level: 1 }]
+// -> [{ level: 1 }, { level: 2 }]
 
 db.push("alllevel", { level: 1 });
+// -> [{ level: 1 }, { level: 2 }, { level: 1 }]
 
 db.pull("alllevel", (e) => e.level === 1);
-// => [{level: 1}, {level: 1}]
+// -> [{level: 1}, {level: 1}]
 
 db.getAll()
-// -> [ { id: 'level', data: [{ level: 1 }, { level: 2 }, {level: 1}] } ]
+// -> [ { id: 'alllevel', data: [{ level: 1 }, { level: 2 }, {level: 1}] } ]
 
 db.pullDelete("alllevel", (e) => e.level === 1)
-// => [{level: 2}]
+// -> [{level: 2}]
 
 db.delete("alllevel");
-// => undefined
+// -> undefined
 
 db.set("level_<userId>", 4);
 // -> 4
@@ -32,3 +32,6 @@ db.add("level_<userId>", 1);
 
 db.subtract("level_<userId>", 7);
 // -> -2
+
+db.deleteAll();
+// -> undefined
